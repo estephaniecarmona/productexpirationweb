e40695d6004b55d38906185473c51d0707752da9
Estephanie Carmona <estephflcl@gmail.com>
2022-08-13 16:43:32 -0500

    update date view

--- django_project/settings.py
+++ django_project/settings.py
@@ -140,8 +140,6 @@ EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
 
 
-print(EMAIL_HOST_USER)
-print(EMAIL_HOST_PASSWORD)
-print(SECRET_KEY)
+
 
 # django_heroku.setting(locals())
